3c6260a895204ced2db6b3acef1f266d613dc482
nirmaldavis <nirmal.davis@gmail.com>
2016-03-01 13:22:08 +0530

    Internal refactoring of factory
    Reafactored to refer function, property from return object of factory
    function

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 var invoiceAppModule = angular.module('invoice1', []);
 
+//Reusable business logic of converting currency 
+//It moved out of controller and made it as a factory
 invoiceAppModule.factory('currencyConvertor', function(){
+    
+    var currencies = ["USD", "EUR", "CNY"];
+    var usdToForiegnRates = { USD: 1,
+                              EUR: 0.74,
+                              CNY: 6.09 };
+    
+    var convert = function(amount, fromCurr, toCurr ) {
+        return amount * usdToForiegnRates[toCurr] / usdToForiegnRates[fromCurr];
+    };
+    
     return {
-        
-        usdToForiegnRates : {
-            USD: 1,
-            EUR: 0.74,
-            CNY: 6.09
-        },
-        convertCurrency : function(amount, fromCurr, toCurr) {
-            return amount * this.usdToForiegnRates[toCurr] / this.usdToForiegnRates[fromCurr];
-        }
+        currencies : currencies,
+        convertCurrency : convert
     };
 });
 
@@ -19,18 +24,8 @@ invoiceAppModule.controller('InvoiceController', function (currencyConvertor) {
     this.quantity = 2;
     this.cost = 24;
     this.fromCurr = "EUR";
-    this.currencies = ["USD", "EUR", "CNY"];
-
-//    this.usdToForiegnRates = {
-//        USD: 1,
-//        EUR: 0.74,
-//        CNY: 6.09
-//    };
-//
-//    this.convertCurrency = function (amount, fromCurr, toCurr) {
-//
-//        return amount * this.usdToForiegnRates[toCurr] / this.usdToForiegnRates[fromCurr];
-//    };
+    
+    this.currencies = currencyConvertor.currencies;
 
     this.total = function (toCurr) {
         return currencyConvertor.convertCurrency(this.quantity * this.cost, this.fromCurr, toCurr);
