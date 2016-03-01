39f279060359402964d89640324e329c54720560
nirmaldavis <nirmal.davis@gmail.com>
2016-03-01 12:27:56 +0530

    With Usage of Factory
    Introducing currency convertor - View-independent business logic:
    Services

--- src/app.js
+++ src/app.js
@@ -1,46 +1,39 @@
-var invoiceAppModule = angular.module('invoice1',[]);
+var invoiceAppModule = angular.module('invoice1', []);
 
-invoiceAppModule.controller('InvoiceController', function() {
+invoiceAppModule.factory('currencyConvertor', function(){
+    return {
+        
+        usdToForiegnRates : {
+            USD: 1,
+            EUR: 0.74,
+            CNY: 6.09
+        },
+        convertCurrency : function(amount, fromCurr, toCurr) {
+            return amount * this.usdToForiegnRates[toCurr] / this.usdToForiegnRates[fromCurr];
+        }
+    };
+});
+
+
+invoiceAppModule.controller('InvoiceController', function (currencyConvertor) {
     this.quantity = 2;
     this.cost = 24;
     this.fromCurr = "EUR";
-    this.currencies = ["USD" , "EUR" , "CNY" ];
-    
-    this.usdToForiegnRates = {  USD : 1,
-                                EUR : 0.74,
-                                CNY : 6.09
-                             };
-    
-    this.convertCurrency = function(amount, fromCurr, toCurr) {
-        
-         return amount * this.usdToForiegnRates[toCurr] / this.usdToForiegnRates[fromCurr];
-    };
-    
-    this.total = function(toCurr) {
-        return this.convertCurrency(this.quantity * this.cost, this.fromCurr, toCurr);
-    };
-                
-});    
+    this.currencies = ["USD", "EUR", "CNY"];
 
-//angular.module('invoice1', [])
-//.controller('InvoiceController', function() {
-//  this.qty = 1;
-//  this.cost = 2;
-//  this.inCurr = 'EUR';
-//  this.currencies = ['USD', 'EUR', 'CNY'];
-//  this.usdToForeignRates = {
-//    USD: 1,
-//    EUR: 0.74,
-//    CNY: 6.09
-//  };
+//    this.usdToForiegnRates = {
+//        USD: 1,
+//        EUR: 0.74,
+//        CNY: 6.09
+//    };
 //
-//  this.total = function total(outCurr) {
-//    return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
-//  };
-//  this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
-//    return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
-//  };
-//  this.pay = function pay() {
-//    window.alert("Thanks!");
-//  };
-//});
+//    this.convertCurrency = function (amount, fromCurr, toCurr) {
+//
+//        return amount * this.usdToForiegnRates[toCurr] / this.usdToForiegnRates[fromCurr];
+//    };
+
+    this.total = function (toCurr) {
+        return currencyConvertor.convertCurrency(this.quantity * this.cost, this.fromCurr, toCurr);
+    };
+
+});
